2fa2659057e8b65ec6bd5bfa330d1af694330217
Mohamed Ellety <elletymohamed@gmail.com>
2026-01-15 21:54:43 +0200

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -19,12 +19,12 @@ CREATE TABLE bronze.crm_sales_visits(
 	doctor_name varchar(50),
 	speciality varchar(50),
 	product_promoted varchar(50),
-	visit_date date,
+	visit_date varchar(50),
 	visit_duration_minutes varchar(50),
 	visit_outcome varchar(50),
 	notes varchar(50),
 	territory_code varchar(50),
-	created_at timestamp
+	created_at varchar(50)
 );
 
 DROP TABLE IF EXISTS bronze.doctors_master;
@@ -37,29 +37,29 @@ CREATE TABLE bronze.doctors_master (
 	city varchar(50),
 	region varchar(50),
 	tier varchar(50),
-	active_flag integer,
-	last_updated date
+	active_flag varchar(50),
+	last_updated varchar(50)
 );
 
 DROP TABLE IF EXISTS bronze.inventory_snapshot;
 
 CREATE TABLE bronze.inventory_snapshot (
-	snapshot_date date,
+	snapshot_date varchar(50),
 	product_id varchar(50),
 	warehouse_id varchar(50),
-	available_qty integer,
-	reserved_qty integer,
-	expiry_date date,
+	available_qty varchar(50),
+	reserved_qty varchar(50),
+	expiry_date varchar(50),
 	batch_number varchar(50)
 );
 
 DROP TABLE IF EXISTS bronze.market_data_external;
 
 CREATE TABLE bronze.market_data_external (
-	MONTH date,
+	MONTH varchar(50),
 	product_name varchar(50),
-	market_sales_value integer,
-	market_sales_units integer,
+	market_sales_value varchar(50),
+	market_sales_units varchar(50),
 	competitor_name varchar(50),
 	region varchar(50),
 	data_source varchar(50)
@@ -73,7 +73,7 @@ CREATE TABLE bronze.pharmacies_master (
 	city varchar(50),
 	region varchar(50),
 	channel varchar(50),
-	active_flag integer
+	active_flag varchar(50)
 );
 
 DROP TABLE IF EXISTS bronze.products_master;
@@ -86,7 +86,7 @@ CREATE TABLE bronze.products_master (
 	indication varchar(50),
 	dosage_form varchar(50),
 	strength varchar(50),
-	launch_date date,
+	launch_date varchar(50),
 	status varchar(50)
 );
 
@@ -97,9 +97,9 @@ CREATE TABLE bronze.promotional_spend (
 	product_id varchar(50),
 	campaign_name varchar(50),
 	channel varchar(50),
-	spend_amount integer,
-	start_date date,
-	end_date date,
+	spend_amount varchar(50),
+	start_date varchar(50),
+	end_date varchar(50),
 	region varchar(50)
 );
 
@@ -108,13 +108,13 @@ DROP TABLE IF EXISTS bronze.sales_transactions;
 CREATE TABLE bronze.sales_transactions (
 	transaction_id varchar(50),
 	product_id varchar(50),
-	product_name varchar(100),
-	quantity integer,
-	unit_price integer,
-	total_value integer,
+	product_name varchar(50),
+	quantity varchar(50),
+	unit_price varchar(50),
+	total_value varchar(50),
 	pharmacy_id varchar(50),
 	pharmacy_name varchar(50),
 	city varchar(50),
-	transaction_date date,
+	transaction_date varchar(50),
 	distributor_id varchar(50)
 );
